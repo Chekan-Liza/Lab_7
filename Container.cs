529be30130cc5aa6482c7c036a8e28578c31845e
Chekan-Liza <liza.chekan@gmail.com>
2020-10-29 09:20:23 +0300

    Container

---
+++ Container.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Bookkeeping
+    {
+
+        public List<Document> data = new List<Document>();
+
+        public static int counter = 0;
+
+        public void Output()
+        {
+            if (counter == 0)
+            {
+                ExccessBookkeepingEmpty err = new ExccessBookkeepingEmpty("ERROR!!!\n Bookkeeping is empty");
+                throw err;
+            }
+            else
+            {
+                foreach (object i in data)
+                {
+                    Console.WriteLine($"Объект {i}");
+                }
+            }
+        }
+
+        public void Remove(Document A)
+        {
+            data.Remove(A);
+        }
+
+        public void ClearLibrary()
+        {
+            if (counter == 0)
+            {
+                ExccessBookkeepingEmpty err = new ExccessBookkeepingEmpty("ERROR!!!\n Bookkeeping is empty");
+                throw err;
+            }
+            else
+            {
+                counter = 0;
+                data.Clear();
+                Console.WriteLine("Бухгалтерия очищена!");
+
+            }
+        }
+
+        public void Push(Document N)
+        {
+            data.Add(N);
+            counter++;
+        }
+    }
+}
